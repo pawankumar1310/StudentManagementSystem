e1e8e0383b9e2f39a172c806d948d116b7c9b93a
pawankumar <pawankumar131098@gmail.com>
2024-03-17 11:15:46 +0530

    update student data by Id

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -64,20 +64,84 @@ namespace StudentManagementSystem.Controllers
                 var getstudent = await _context.Students.FindAsync(student.StudentId);
                 if (getstudent != null)
                 {
-                    _context.SaveChanges();
+                    await _context.SaveChangesAsync();
                     return BadRequest("Already student exist.");
                 }
                 else
                 {
                     _context.Students.Add(student);
-                    _context.SaveChanges();
+                    await _context.SaveChangesAsync();
                     return Ok("Student added successfully !!");
                 }
             }
             catch (Exception ex)
             {
-                _logger.LogError("Failed to insert new student.", ex);
+                _logger.LogError(ex, "Failed to insert new student.");
                 return BadRequest("Failed to insert new student");
+            }
+        }
+
+        //Update Student data
+        //Put :api/students/{id}
+        [HttpPut("UpdateStudentData")]
+        public async Task<IActionResult> UpdateStudentData([FromBody] Student student)
+        {
+            try
+            {
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+                return Ok("Updated student details  successfully!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update student details");
+                return BadRequest("Failed to update the student details");
+            }
+        }
+
+        [HttpPut("{id}/UpdateStudentByID", Name = "UpdateStudentByID")]
+        public async Task<IActionResult> UpdateStudentDataByID(string id,[FromBody] Student updatedStudent)
+        {
+            try
+            {
+                var existingStudent = await _context.Students.FindAsync(id);
+                if (existingStudent != null)
+                {
+                    // Update all properties
+                    //existingStudent.StudentId = updatedStudent.StudentId;
+                    existingStudent.Gender = updatedStudent.Gender;
+                    existingStudent.NationalIty = updatedStudent.NationalIty;
+                    existingStudent.PlaceofBirth = updatedStudent.PlaceofBirth;
+                    existingStudent.StageId = updatedStudent.StageId;
+                    existingStudent.GradeId = updatedStudent.GradeId;
+                    existingStudent.SectionId = updatedStudent.SectionId;
+                    existingStudent.Topic = updatedStudent.Topic;
+                    existingStudent.Semester = updatedStudent.Semester;
+                    existingStudent.Relation = updatedStudent.Relation;
+                    existingStudent.Raisedhands = updatedStudent.Raisedhands;
+                    existingStudent.VisItedResources = updatedStudent.VisItedResources;
+                    existingStudent.AnnouncementsView = updatedStudent.AnnouncementsView;
+                    existingStudent.Discussion = updatedStudent.Discussion;
+                    existingStudent.ParentAnsweringSurvey = updatedStudent.ParentAnsweringSurvey;
+                    existingStudent.ParentschoolSatisfaction =  updatedStudent.ParentschoolSatisfaction;
+                    existingStudent.StudentAbsenceDays = updatedStudent.StudentAbsenceDays;
+                    existingStudent.StudentMarks = updatedStudent.StudentMarks;
+                    existingStudent.Class = updatedStudent.Class;
+
+                    _context.Update(existingStudent);
+                    await _context.SaveChangesAsync();
+
+                    return Ok("Updated student details successfully!");
+                }
+                else
+                {
+                    return NotFound("Student not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update student details");
+                return BadRequest("Failed to update the student details");
             }
         }
     }
